cecbb18768c389b9f04f41817af309c38ab2cd34
CatUnderGlue <warmonds1996@gmail.com>
2022-11-28 17:34:39 +0500

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Домашнее задание
+        // Задание 1
+        System.out.println("Задание 1");
+        int firstIntVar = 2147483647;
+        byte firstByteVar = 127;
+        short firstShortVar = 32767;
+        long firstLongVar = 9223372036854775807L;
+        float firstFloatVar = 22.2f;
+        double firstDoubleVar = 22.2;
+        System.out.println("Значение переменной int: " + firstIntVar);
+        System.out.println("Значение переменной byte: " + firstByteVar);
+        System.out.println("Значение переменной short: " + firstShortVar);
+        System.out.println("Значение переменной long: " + firstLongVar);
+        System.out.println("Значение переменной float: " + firstFloatVar);
+        System.out.println("Значение переменной double: " + firstDoubleVar);
     }
 }
